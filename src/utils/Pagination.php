5abf426ebd7217a34de028e049014b52a11e0c1d
Edney Mesquita <edneyphp@outlook.com>
2019-05-15 21:52:45 -0300

    Adicionado suporte a Collections

--- src/utils/Pagination.php
+++ src/utils/Pagination.php
@@ -2,25 +2,46 @@
 
 namespace Odin\utils;
 
+use Odin\utils\collections\{ArrayList, Dictionary};
+
 class Pagination 
 {
     private static $length = 10;
     
-    public static function make(array $itens, int $page = 1, string $varname = "results", bool $reverse = false)
+    public static function make($itens, int $page = 1, string $varname = "results", bool $reverse = false)
+    {
+        $result = null;
+        if(!$itens instanceof ArrayList && !$itens instanceof Dictionary && is_array($itens)){
+            $length = self::$length;
+            $total = ceil(count($itens) / $length);
+            $offset = ($page - 1) * $length;
+
+            if($reverse)
+                $itens = array_reverse($itens);
+            $result = [
+                "{$varname}" => array_slice($itens, $offset, $length),
+                "total" => $total,
+                "current" => $page
+            ];
+        }else{
+            $result = self::collection($itens, $page, $varname, $reverse);
+        }
+        
+        return $result;
+    }
+    
+    protected static function collection($collection, $page, $varname)
     {
         $length = self::$length;
-        $total = ceil(count($itens) / $length);
+        $total = ceil($collection->size() / $length);
         $offset = ($page - 1) * $length;
-        
-        if($reverse)
-            $itens = array_reverse($itens);
+
         $result = [
-            "{$varname}" => array_slice($itens, $offset, $length),
+            "{$varname}" => $collection->slice($offset, $length),
             "total" => $total,
             "current" => $page
         ];
-        
-        return $result;
+            return $result;
     }
     
     public static function setLength(int $length)
