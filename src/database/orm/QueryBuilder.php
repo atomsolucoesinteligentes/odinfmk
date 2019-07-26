b5ad957f2ef00153cf34838a8aec4235c8dd17a3
EdneyMesquita <edneyphp@outlook.com>
2019-07-26 10:27:05 -0300

    Adicionado condição rowCount

--- src/database/orm/QueryBuilder.php
+++ src/database/orm/QueryBuilder.php
@@ -36,7 +36,10 @@ class QueryBuilder
         $query = Filter::clear(trim($this->query));
         $stmt = $this->connection->query($this->query);
         if (strpos($query, "SELECT") === 0) {
-            return $this->generateRegister($stmt->fetchAll(\PDO::FETCH_OBJ));
+            if($stmt->rowCount > 0) {
+                return $this->generateRegister($stmt->fetchAll(\PDO::FETCH_OBJ));
+            }
+            return false;
         } else {
             return $stmt;
         }
